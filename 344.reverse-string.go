90dff2c480a83a0efec6b799c7a2cbffab435e67
William Gu <williamgusien@gmail.com>
2019-08-13 09:34:44 -0700

    reverse string

---
+++ 344.reverse-string.go
@@ -0,0 +1,48 @@
+/*
+ * @lc app=leetcode id=344 lang=golang
+ *
+ * [344] Reverse String
+ *
+ * https://leetcode.com/problems/reverse-string/description/
+ *
+ * algorithms
+ * Easy (63.74%)
+ * Total Accepted:    471.7K
+ * Total Submissions: 739.5K
+ * Testcase Example:  '["h","e","l","l","o"]'
+ *
+ * Write a function that reverses a string. The input string is given as an
+ * array of characters char[].
+ *
+ * Do not allocate extra space for another array, you must do this by modifying
+ * the input array in-place with O(1) extra memory.
+ *
+ * You may assume all the characters consist of printable ascii
+ * characters.
+ *
+ *
+ *
+ *
+ * Example 1:
+ *
+ *
+ * Input: ["h","e","l","l","o"]
+ * Output: ["o","l","l","e","h"]
+ *
+ *
+ *
+ * Example 2:
+ *
+ *
+ * Input: ["H","a","n","n","a","h"]
+ * Output: ["h","a","n","n","a","H"]
+ *
+ *
+ *
+ *
+ */
+func reverseString(s []byte) {
+	for i, j := 0, len(s)-1; i < j; i, j = i+1, j-1 {
+		s[i], s[j] = s[j], s[i]
+	}
+}
